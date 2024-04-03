a12bbdc4de788f7c926230507ccbfee95d0a6a25
Kevin Torrington <kevin.torrington@accenture.com>
2024-04-03 23:47:59 +0100

    Headers set, before adding nested.

--- src/index.js
+++ src/index.js
@@ -11,22 +11,34 @@ const example = document.getElementById("handsontable");
 new Handsontable(example, {
   data,
   height: 450,
-  colWidths: [170, 156, 222, 130, 130, 120, 120],
-  colHeaders: ["original_reading_ct_mwh", "dqi", "rule_value", "settlement_date", "rule_type", "settlement_period", "settlement_start_datetime", "actual_estimate_indicator", "rule_description", "rule_created_date", "rule_effective_from", "rule_effective_to", "rule_id", "weekday"],
+  colWidths: [100, 54, 100, 100, 100, 74, 120, 145, 70, 70, 70, 100, 150],
+  colHeaders: ["Date", "DQI", "Original", "Corrected", "Proposed", "Rule ID", "Rule", "Description", "Value", "Period", "Actual", "Created", "Effective"],
+  // nestedHeaders: [
+  //   [{label: 'Group A', colspan: 2}, {label: 'B-C', colspan: 2}, 'D'],
+  //   ['E', 'F', 'G', 'H']
+  // ],
+  
   columns: [
-    { data: 1, type: "text" },
-    { data: 2, type: "text" },
-    { data: 3, type: "text" },
-    { data: 4, type: "text" },
-    { data: 5, type: "text" },
-    { data: 6, type: "text" },
-    { data: 7, type: "text" },
-    { data: 8, type: "text" },
-    { data: 9, type: "text" },
-    { data: 10, type: "text" },
-    { data: 11, type: "text" },
-    { data: 12, type: "text" },
-    { data: 13, type: "text" },
+
+    // Users rated as critical
+    { data: 0, type: "date" }, // Date
+    { data: 1, type: "text" }, // DQI
+    { data: 2, type: "text" }, // Original Read (MHw)
+    { data: 3, type: "text" }, // Corrected Read (MHw)
+    { data: 4, type: "text" }, // Proposed (MHw)
+    { data: 5, type: "text" }, // Rule ID
+    { data: 6, type: "text" }, // Rule
+    { data: 7, type: "text" }, // Rule Description
+
+    // Users reated as important
+    { data: 8, type: "text" }, // Rule Value
+    { data: 9, type: "text" }, // Period
+    // { data: 10, type: "text" }, // Start date
+    // { data: 11, type: "text" }, // Start time
+    { data: 12, type: "text" }, // Actual
+    { data: 13, type: "text" }, // Created
+    { data: 14, type: "text" }, // Effective
+    // { data: 15, type: "text" }, // weekday
   
   ],
   dropdownMenu: true,
